30e827847aa6f15c5e47db902b3edb9ed23f7db7
FlyingJester <foolkingcrown@gmail.com>
2016-04-23 19:04:59 -0700

    Added missing files for Sapphire

---
+++ src/plugins/sapphire/sdl2_keys.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "keys.hpp"
+
+#if SAPPHIRE_USE_SDL2
+
+namespace Sapphire {
+
+    extern const std::array<unsigned, SAPPHIRE_NUM_KEYS> key_numbers;
+
+} // namespace Sapphire
+
+#endif
